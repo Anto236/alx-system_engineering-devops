bdb66125667b787085c772a315a60e2fcb9d4500
Anto236 <Antonymbugua1237@gmail.com>
2023-01-31 15:28:37 +0000

    task 6

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -6,7 +6,7 @@
 argument = ARGV[0]
 
 #regex pattern starts with digit and range of 10
-pattern = /^\d{10}/
+pattern = /^\d{10}$/
 
 #scan for instances of the pattern
 result = argument.scan(pattern)
