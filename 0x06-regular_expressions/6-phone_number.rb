6de5d8f3ca0fdd2be56378b5266dd0dcf859de4a
Anto236 <Antonymbugua1237@gmail.com>
2023-01-31 15:27:58 +0000

    task 6

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+#htn and hbtn
+
+#Accept the argument
+argument = ARGV[0]
+
+#regex pattern starts with digit and range of 10
+pattern = /^\d{10}/
+
+#scan for instances of the pattern
+result = argument.scan(pattern)
+
+#print result
+puts result.join
