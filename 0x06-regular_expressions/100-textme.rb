98b8f5bdc6b7d39593100e05df548fec6c8243de
Anto236 <Antonymbugua1237@gmail.com>
2023-01-31 15:53:22 +0000

    task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
-def extract_info(input_string)
-	sender = input_string[/from:(.*?)\s/, 1]
-	receiver = input_string[/to:(.*?)\s/, 1]
-	flags = input_string[/flags:(.*?)\s/, 1]
-	"#{sender},#{receiver},#{flags}"
-end
 
-input_string = ARGV[0]
-puts extract_info(input_string)
+#htn and hbtn
 
+#Accept the argument
+argument = ARGV[0]
+
+#regex pattern
+pattern = /(?<=from:|to:|flags:).+?(?=\])/
+
+#scan for instances of the pattern
+result = argument.scan(pattern)
+
+#print result
+puts result.join(',')
