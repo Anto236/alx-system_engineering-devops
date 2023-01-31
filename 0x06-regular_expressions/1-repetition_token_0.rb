c696d5afa27bb685857706a4d337a450a519378e
Anto236 <Antonymbugua1237@gmail.com>
2023-01-31 14:55:40 +0000

    task 1

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+#expression to match hbtttn
+
+# Accept the argument
+argument = ARGV[0]
+
+# Define the regex pattern
+pattern = /hbt{2,3}n/
+
+# Scan method to find instance of pattern
+result = argument.scan(pattern)
+
+# Print result and join array to string
+puts result.join
