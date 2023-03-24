26f06ce7efc051b26dc13e96b5f01cb5178dfd82
Anto236 <Antonymbugua1237@gmail.com>
2023-03-24 11:11:11 +0000

    task 2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# create a manifest that kills a process named killmenow.
+
+exec { 'pkill_killmenow_process':
+  command => '/usr/bin/pkill killmenow',
+  path    => ['/usr/bin', '/usr/local/bin', '/bin'],
+}
