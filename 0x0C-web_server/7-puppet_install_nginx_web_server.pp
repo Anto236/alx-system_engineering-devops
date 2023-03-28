b805429342e4b21bc4b1a53cf20700d6b688b3c1
Anto236 <Antonymbugua1237@gmail.com>
2023-03-28 21:24:56 +0000

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,42 +1,20 @@
-# Define a class for installing and configuring Nginx
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
+# install and configure an Nginx server using Puppet instead of Bash
 
-  file { '/var/www/html/index.html':
-    content => 'Hello World!',
-  }
-
-  file { '/var/www/html/404.html':
-    content => "Ceci n'est pas une page",
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    content => template('nginx/default.erb'),
-  }
-
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx'
 }
-
-# Define a template for the Nginx configuration file
-# that includes a redirect for /redirect_me
-# and a custom 404 page
-file { '/etc/nginx/sites-available/default':
-  content => template('nginx/default.erb'),
+file { 'path_to_html':
+  content => 'Holberton School',
+  path    => '/var/www/html/index.nginx-debian.html'
 }
-
-# Set up the redirect for /redirect_me
-nginx::config { 'redirect':
-  from   => '/',
-  to     => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
-  status => 301,
+file_line { 'put_in_line':
+  ensure => present,
+  path   => 'etc/nginx/sites-available/default',
+  after  => '/listen 80 default_server;'
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
-
-# Define the Nginx class as the default class to be applied to the node
-node default {
-  include nginx
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx']
 }
