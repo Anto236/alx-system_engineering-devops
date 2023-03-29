9be9d93df565596dc6aa769ea684d4640738dcee
Anto236 <Antonymbugua1237@gmail.com>
2023-03-29 21:58:19 +0000

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,39 @@
-# Installs a Nginx server
+# Install Nginx
+package { 'nginx':
+  ensure => installed,
+}
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Anto236 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+# Ensure Nginx service is running and enabled on boot
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
+
+# Configure Nginx server
+file { '/etc/nginx/sites-available/default':
+  ensure => file,
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    # Redirect /redirect_me
+    if ($request_uri = /redirect_me) {
+        return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    # Handle root request
+    location / {
+        return 200 "Hello World!";
+    }
+}
+',
+  notify => Service['nginx'],
+}
+
+# Restart Nginx service after configuration changes
+exec { 'nginx-restart':
+  command => 'sudo service nginx restart',
+  refreshonly => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
