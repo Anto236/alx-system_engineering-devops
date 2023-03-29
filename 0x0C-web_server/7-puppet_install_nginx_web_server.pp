72538f38c16ae9a98c7571922d1d4c3d7af05058
Anto236 <Antonymbugua1237@gmail.com>
2023-03-29 21:34:29 +0000

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,32 @@
-#Install Nginx web server (w/ Puppet)
+# Install Nginx
+package { 'nginx':
+  ensure => installed,
+}
 
-class nginx_server {
+# Ensure Nginx service is running and enabled on boot
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
 
-  # Install Nginx
-  package { 'nginx':
-    ensure => installed,
-  }
+# Configure Nginx server
+file { '/etc/nginx/sites-available/default':
+  ensure => file,
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-  # Ensure Nginx service is running and enabled on boot
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
-
-  # Configure Nginx server
-  file { '/etc/nginx/sites-available/default':
-    ensure => file,
-    content => '
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        # Redirect /redirect_me
-        if ($request_uri = /redirect_me) {
-            return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-
-        # Handle root request
-        location / {
-            return 200 "Hello World!";
-        }
+    # Redirect /redirect_me
+    if ($request_uri = /redirect_me) {
+        return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }
-    ',
-    notify => Service['nginx'],
-  }
 
+    # Handle root request
+    location / {
+        return 200 "Hello World!";
+    }
+}
+',
+  notify => Service['nginx'],
 }
