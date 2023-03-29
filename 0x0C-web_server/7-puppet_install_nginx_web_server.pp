7168112aed8c54cfc4240275c4cac33eef5f824b
Anto236 <Antonymbugua1237@gmail.com>
2023-03-29 21:23:00 +0000

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,32 @@
-# install and configure an Nginx server using Puppet instead of Bash
-
-exec { 'apt-update':
-  command => '/usr/bin/apt-get update',
-}
-
+# Install Nginx
 package { 'nginx':
   ensure => installed,
-  name   => 'nginx'
 }
 
-file { '/var/www/html/index.nginx-debian.html':
-  content => 'Holberton School',
-}
-
-file_line { 'put_in_line':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => '/listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
+# Ensure Nginx service is running and enabled on boot
 service { 'nginx':
-  ensure  => running,
-  require => [Package['nginx'], Exec['apt-update']],
+  ensure => running,
+  enable => true,
+}
+
+# Configure Nginx server
+file { '/etc/nginx/sites-available/default':
+  ensure => file,
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    # Redirect /redirect_me
+    if ($request_uri = /redirect_me) {
+        return 301  https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    # Handle root request
+    location / {
+        return 200 "Hello World!";
+    }
+}
+',
+  notify => Service['nginx'],
 }
