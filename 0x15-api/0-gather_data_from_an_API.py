6f89fd1ba5e8d11fd2d0677ce062302ae152eed7
Anto236 <Antonymbugua1237@gmail.com>
2023-04-24 19:46:16 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
-"""This script returns information about his/her
-todo list progress"""
-import requests as rq
-from sys import argv
+"""get data from jsonplaceholder"""
+import requests
+import sys
 
+if __name__ == '__main__':
+    """REST API manipulations"""
+    if len(sys.argv) > 1 and isinstance(eval(sys.argv[1]), int):
+        pass
+    else:
+        sys.exit(0)
 
-def tasks_done(todos, mode='count'):
-    """Return the number of completed tasks or print the titles"""
-    done = 0
-    for todo in todos:
-        if todo.get('completed', False):
-            if mode == 'title':
-                print("\t " + todo['title'])
-            else:
-                done += 1
-    return done
+    BASE_API = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    user_response_url = BASE_API + "users/{}".format(employee_id)
+    todo_response_url = BASE_API + "users/{}/todos".format(employee_id)
 
+    user_response = requests.get(user_response_url).json()
+    todo_response = requests.get(todo_response_url).json()
 
-user_id = argv[1] if argv[1:] else 1
-url = 'https://jsonplaceholder.typicode.com/'
-user = rq.get(url + 'users/{}'.format(user_id)).json()
-todos = rq.get(url + 'users/{}/todos'.format(user_id)).json()
-print('Employee {} is done with tasks({}/{}):'.
-      format(user.get('name'), tasks_done(todos), len(todos)))
-tasks_done(todos, 'title')
+    employee_name = user_response.get('name')
+
+    num_tasks_done = 0
+    for todo in todo_response:
+        if todo.get("completed"):
+            num_tasks_done += 1
+
+    total_num_tasks = 0
+    for todo in todo_response:
+        total_num_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          num_tasks_done, total_num_tasks))
+
+    for todo in todo_response:
+        task_title = todo.get("title")
+        if (todo.get("completed")):
+            print("\t {}".format(task_title))
