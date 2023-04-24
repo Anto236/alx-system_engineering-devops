7a378f49bc448782e2104928774ef5a89cb86815
Anto236 <Antonymbugua1237@gmail.com>
2023-04-24 19:38:34 +0000

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" Uses a REST API, fetching data about a given employee
-    (identified by their id passed to sript as argument) &
-    exports the data (of all tasks owned by user) in the CSV format
 """
-import csv
-import requests
-import sys
-
+Uses a REST API, fetching data about a given employee
+(identified by their id passed to the script as an argument) and
+exports the data (of all tasks owned by the user) in the CSV format.
+"""
 
 if __name__ == '__main__':
-    # get the data
+    import csv
+    import requests
+    import sys
+
     if len(sys.argv) > 1:
         api = 'https://jsonplaceholder.typicode.com/'
         user = requests.get(api + 'users/{}'.format(sys.argv[1]))
@@ -18,14 +18,13 @@ if __name__ == '__main__':
         for todo in todos.json():
             if todo.get('userId') == int(sys.argv[1]):
                 user_todos.append(todo)
-        # print(user_todos)
+
         file_name = '{}.csv'.format(sys.argv[1])
         with open(file_name, 'w', newline='') as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
             for todo in user_todos:
-                row = []
-                row.extend([sys.argv[1], user.json().get('username')])
-                row.extend([todo.get('completed'), todo.get('title')])
+                row = [sys.argv[1], user.json().get('username'),
+                       todo.get('completed'), todo.get('title')]
                 writer.writerow(row)
     else:
         pass
