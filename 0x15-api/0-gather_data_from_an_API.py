07a772f0f731732dc7ad8f37b3eaff13afae4d61
Anto236 <Antonymbugua1237@gmail.com>
2023-04-24 16:45:04 +0000

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 """
-Uses the JSON placeholder api to query data about an employee
+Using REST API, for a given employee ID, returns information about
+his/her TODO list progress.
 """
 
-from requests import get
-from sys import argv
-
 if __name__ == '__main__':
-    main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + "/user/{}/todos".format(argv[1])
-    name_url = main_url + "/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    from requests import get
+    from sys import argv
 
-    todo_num = len(todo_result)
-    todo_complete = len([todo for todo in todo_result
-                         if todo.get("completed")])
-    name = name_result.get("name")
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, todo_complete, todo_num))
-    for todo in todo_result:
-        if (todo.get("completed")):
-            print("\t {}".format(todo.get("title")))
+    #  get args
+    userId = int(argv[1])
+
+    #  Fetch todos and user
+    todos = get('https://jsonplaceholder.typicode.com/todos').json()
+    user = get('https://jsonplaceholder.typicode.com/users/{}'.format(
+               userId)).json()
+
+    #  Extract user specific todos
+    user_todos = []
+    for todo in todos:
+        if todo['userId'] == userId:
+            user_todos.append(todo)
+
+    #  get todos analytics
+    todos_total = len(user_todos)
+    todos_completed = sum(todo['completed'] for todo in user_todos)
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+          user['name'], todos_completed, todos_total))
+    for todo in [todo for todo in user_todos if todo['completed']]:
+        print('\t {}'.format(todo['title']))
