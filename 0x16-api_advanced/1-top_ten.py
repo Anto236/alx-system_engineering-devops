455a4fc964412a0685cd0f52616d2be1a97bc99e
Anto236 <Antonymbugua1237@gmail.com>
2023-05-09 15:00:22 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit.
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Custom User-Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children'][:10]:
+            print(post['data']['title'])
+    else:
+        print("None")
