74e001e72c9e58af768e3560cf8ff796c57948b0
Anto236 <Antonymbugua1237@gmail.com>
2023-05-09 15:08:06 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""
-a function that queries the Reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit.
-"""
-
-import requests
+"""function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit."""
 
 
 def top_ten(subreddit):
-    """ Queries to Reddit API """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Custom User-Agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        for post in data['data']['children'][:10]:
-            print(post['data']['title'])
-    else:
+    """function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
+    import requests
+    import sys
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
         print("None")
